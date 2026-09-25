54c756af3fc2950f4a53fd3a61b2500284139204
guojunyao <guojunyao181818@163.com>
2026-09-25 20:01:21 +0800

    练习

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-    for (int i=1;i<=100;++i)
-    {
+    double e=0;
+    int i,j;
+    double a;
+    i=0;
+    for (i=0;i<=500;++i) {
+        a=1;
+        j=1;
+        for (j=1;j<=i;++j)
+            a*=j;
+        e+=1/a;
     }
+    cout<<e<<endl;
 }
